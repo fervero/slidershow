9a5f5bcad900e08b726e696f273e43d0c072404e
Maciej Bojko <maciej.bojko@gmail.com>
2017-05-14 17:47:49 +0200

    Completed and combined horizontal/vertical flip, forward and backward.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,58 @@
 $(function() {
-    function verticalFlip(slider) {
-        var currentImg = 0,
-            $imgs = slider.find('.slider-img').addClass('collated'),
+    var animationEvent = (function whichAnimationEvent(){
+        var t,
+            el = document.createElement("fakeelement"),
+            animations = {
+            "animation"      : "animationend",
+            "OAnimation"     : "oAnimationEnd",
+            "MozAnimation"   : "animationend",
+            "WebkitAnimation": "webkitAnimationEnd"
+            }
+        for (t in animations)
+            if (el.style[t] !== undefined)
+                return animations[t];
+        return false;
+    })(); 
+    
+    function flip(slider) {
+        var $imgs = $.map(slider.find('.slider-img').addClass('collated'), function(x) {return $(x)}),
             sliderLength = $imgs.length,
-            $buttonFlip = slider.find('.btn-flip'),
+            $buttonForward = slider.find('.btn-forward'),
+            $buttonBackward = slider.find('.btn-backward'),
             sliderWindow = slider.find('.slider-window'),
+            direction = slider.data('direction') || 'horizontal',
+            flipClasses = {
+                horizontal: ['horizontally-flipped',
+                               'horizontally-flipped-forward'],
+                vertical: [
+                    'vertically-flipped',
+                    'vertically-flipped-forward'
+                ]
+            },
+            forwardClass = flipClasses[direction][1],
+            reverseClass = flipClasses[direction][0];
             nextImg = function() {
                 if ($imgs.length < 1) return;
-                sliderWindow.append(
-                    $imgs.eq( currentImg = ( currentImg + 1) % sliderLength ).addClass('vertically-flipped')
-                );
-            }
-        slider.addClass('slider-running');
-        $buttonFlip.on('click', nextImg);
-    }
-    function horizontalFlip(slider) {
-        var currentImg = 0,
-            $imgs = slider.find('.slider-img').addClass('collated'),
-            sliderLength = $imgs.length,
-            $buttonFlip = slider.find('.btn-flip'),
-            sliderWindow = slider.find('.slider-window'),
-            nextImg = function() {
+                var $flippedImg = $imgs.pop();
+                $flippedImg.addClass(forwardClass).one(animationEvent, function(e) {
+                    var $this = $(this).removeClass(forwardClass);
+                    $imgs.unshift($this);
+                    sliderWindow.prepend($this);
+                });
+            },
+            prevImg = function() {
                 if ($imgs.length < 1) return;
-                sliderWindow.append(
-                    $imgs.eq( currentImg = ( currentImg + 1) % sliderLength ).addClass('horizontally-flipped')
-                );
+                var $flippedImg = $imgs.shift();
+                sliderWindow.append($flippedImg.addClass(reverseClass));
+                $flippedImg.one(animationEvent, function(e) {
+                    var $this = $(this).removeClass(reverseClass);
+                    $imgs.push($this);
+                });
             }
         slider.addClass('slider-running');
-        $buttonFlip.on('click', nextImg);
+        $buttonForward.on('click', nextImg);
+        $buttonBackward.on('click', prevImg);
     }
-    
-    
-    verticalFlip($(".slider-flip-vert"));
-    horizontalFlip($(".slider-flip-horiz"));
+    flip($(".slider-flip").last());
+    flip($(".slider-flip").first());
 });
